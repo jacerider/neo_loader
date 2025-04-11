4e0652b3d3cff830e1e18fde144938a53ebb5670
JaceRider <jacerider@gmail.com>
2025-04-11 12:19:48 -0500

    Pass in destination to avoid issues when forms or elements with destination providers are loaded via ajax.

--- src/js/ajax.ts
+++ src/js/ajax.ts
@@ -8,16 +8,20 @@
         var observer = new IntersectionObserver((entries, observer) => {
           entries.forEach(entry => {
             if (entry.intersectionRatio > 0) {
-              observer.disconnect();
-              const options = {
-                url: element.dataset.loaderUrl,
-                progress: false,
-                wrapper: element.id,
-                effect: 'fade',
-                httpMethod: 'GET',
-              } as any;
-              const ajax = Drupal.ajax(options) as any;
-              ajax.execute();
+              let url = element.dataset.loaderUrl;
+              if (url) {
+                url = addDestination(url);
+                observer.disconnect();
+                const options = {
+                  url: url,
+                  progress: false,
+                  wrapper: element.id,
+                  effect: 'fade',
+                  httpMethod: 'GET',
+                } as any;
+                const ajax = Drupal.ajax(options) as any;
+                ajax.execute();
+              }
             }
           });
         });
@@ -26,6 +30,30 @@
       });
     }
   };
+
+  /**
+   * Adds a destination parameter to the URL if it doesn't already exist.
+   *
+   * @param {string} url - The URL to which the destination parameter will be added.
+   * @returns {string} - The modified URL with the destination parameter.
+   */
+  const addDestination = (url:string):string => {
+    const parts = url.split('?');
+    let currentDestination = null;
+    if (parts[1]) {
+      currentDestination = new URLSearchParams('?' + parts[1]).get('destination');
+    }
+    if (!currentDestination) {
+      let key = 'destination';
+      let value = window.location.pathname;
+      key = encodeURI(key);
+      value = encodeURI(value);
+      var isQuestionMarkPresent = url && url.indexOf('?') !== -1,
+        separator = isQuestionMarkPresent ? '&' : '?';
+      url += separator + key + '=' + value;
+    }
+    return url;
+  }
 
 })(Drupal);
 
