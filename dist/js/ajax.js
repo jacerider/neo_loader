fddfb3b053a809ac69ce7610c20af570de4b6eea
JaceRider <jacerider@gmail.com>
2025-04-15 09:16:34 -0500

    Compiled source.

--- dist/js/ajax.js
+++ dist/js/ajax.js
@@ -1,25 +1,39 @@
-(function(e) {
-  e.behaviors.neoLoaderAjax = {
-    attach: (s) => {
-      once("neo-loader-ajax", "[data-loader-url]").forEach((a) => {
-        var o = new IntersectionObserver((r, t) => {
-          r.forEach((c) => {
-            if (c.intersectionRatio > 0) {
-              t.disconnect();
-              const n = {
-                url: a.dataset.loaderUrl,
-                progress: !1,
-                wrapper: a.id,
-                effect: "fade",
-                httpMethod: "GET"
-              };
-              e.ajax(n).execute();
+(function(s) {
+  s.behaviors.neoLoaderAjax = {
+    attach: (a) => {
+      once("neo-loader-ajax", "[data-loader-url]").forEach((t) => {
+        var n = new IntersectionObserver((r, i) => {
+          r.forEach((o) => {
+            if (o.intersectionRatio > 0) {
+              let e = t.dataset.loaderUrl;
+              if (e) {
+                e = c(e), i.disconnect();
+                const d = {
+                  url: e,
+                  progress: !1,
+                  wrapper: t.id,
+                  effect: "fade",
+                  httpMethod: "GET"
+                };
+                s.ajax(d).execute();
+              }
             }
           });
         });
-        o.observe(a);
+        n.observe(t);
       });
     }
+  };
+  const c = (a) => {
+    const t = a.split("?");
+    let n = null;
+    if (t[1] && (n = new URLSearchParams("?" + t[1]).get("destination")), !n) {
+      let o = "destination", e = window.location.pathname;
+      o = encodeURI(o), e = encodeURI(e);
+      var r = a && a.indexOf("?") !== -1, i = r ? "&" : "?";
+      a += i + o + "=" + e;
+    }
+    return a;
   };
 })(Drupal);
 //# sourceMappingURL=ajax.js.map
