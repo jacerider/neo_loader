b2569f1a997a7402e3f952f141c7b820a41a7fd2
JaceRider <jacerider@gmail.com>
2025-05-13 15:34:16 -0500

    Compiled.

--- dist/js/loader.js
+++ dist/js/loader.js
@@ -1,60 +1,51 @@
-(function(f, u, d) {
-  let c = !1, l = null;
-  f.behaviors.neoLoader = {
-    attach: function(o) {
-      u("neo-loader", ".use-neo-loader", o).forEach((e) => {
+(function(c, u, i) {
+  let d = null;
+  c.behaviors.neoLoader = {
+    attach: function(n) {
+      u("neo-loader", ".use-neo-loader", n).forEach((e) => {
         e.addEventListener("click", (a) => {
-          const s = e.getAttribute("data-neo-loader-message") || f.t("Loading..."), r = parseInt(e.getAttribute("data-neo-loader-type") || "fullscreen"), t = parseInt(e.getAttribute("data-neo-loader-delay") || "0");
+          const s = e.getAttribute("data-neo-loader-message") || c.t("Loading..."), r = parseInt(e.getAttribute("data-neo-loader-type") || "fullscreen"), t = parseInt(e.getAttribute("data-neo-loader-delay") || "0");
           this.show(s, r, e, t);
         });
       });
     },
-    show: (o, e, a, s) => {
+    show: (n, e, a, s) => {
       var r;
-      if (e = e || "fullscreen", a = a || "body", s = Math.max(typeof s > "u" ? 200 : s, 10), typeof d.neoLoader < "u" && typeof d.neoLoader.markup < "u") {
+      if (e = e || "fullscreen", a = a || "body", s = Math.max(typeof s > "u" ? 0 : s, 10), typeof i.neoLoader < "u" && typeof i.neoLoader.markup < "u") {
         const t = document.createElement("div");
-        if (t.classList.add("ajax-progress"), t.classList.add("ajax-progress-" + e), t.innerHTML = d.neoLoader.markup, o) {
-          const n = document.createElement("div");
-          n.classList.add("message"), n.textContent = o, (r = t.querySelector(".neo-loader")) == null || r.appendChild(n);
+        if (t.classList.add("ajax-progress"), t.classList.add("ajax-progress-" + e), t.innerHTML = i.neoLoader.markup, n) {
+          const o = document.createElement("div");
+          o.classList.add("message"), o.textContent = n, (r = t.querySelector(".neo-loader")) == null || r.appendChild(o);
         }
         if (a) {
-          const n = typeof a == "string" ? document.querySelector(a) : a;
-          if (n) {
+          const o = typeof a == "string" ? document.querySelector(a) : a;
+          if (o) {
             if (e === "fullscreen")
-              n.appendChild(t);
+              o.appendChild(t);
             else {
-              let i = n.closest(".js-form-item, form");
-              i && i.classList.add("ajax-progress-wrapper"), n.after(t);
+              let l = o.closest(".js-form-item, form");
+              l && l.classList.add("ajax-progress-wrapper"), o.after(t);
             }
-            return l = setTimeout(() => {
-              c = !1;
-              const i = (L) => {
-                L.target.classList.contains("ajax-progress") && (c = !0);
-              };
-              t.addEventListener("transitionend", i), t.classList.add("active");
+            return d = setTimeout(() => {
+              t.classList.add("active");
             }, s), t;
           }
         }
       }
       return null;
     },
-    hide: (o) => {
-      l && clearTimeout(l);
+    hide: (n) => {
+      d && clearTimeout(d);
       const e = document.querySelector(".ajax-progress");
       if (e) {
         if (e.classList.contains("active")) {
-          const a = setInterval(() => {
-            if (c) {
-              clearInterval(a);
-              const s = (r) => {
-                r.target.classList.contains("ajax-progress") && e.remove();
-              };
-              e.addEventListener("transitionend", s), e.classList.remove("active");
-            }
-          }, 10);
+          const a = (s) => {
+            s.target.classList.contains("ajax-progress") && e.remove();
+          };
+          e.addEventListener("transitionend", a), e.classList.remove("active");
         } else
           e.remove();
-        return o && o(), e;
+        return n && n(), e;
       }
       return null;
     }
